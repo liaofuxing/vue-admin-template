3941f31f103690d4f4fdad32a8bf08863853d838
无情的搬砖工 <597429743@qq.com>
2020-04-10 23:13:42 +0800

    代码生成器v0.0.0.1

---
+++ src/api/generateCode.js
@@ -0,0 +1,49 @@
+import request from '@/utils/request'
+import { getToken } from '@/utils/auth'
+
+export function getMysqlTableSelect() {
+  return request({
+    url: '/generate/generateCode/getMysqlTableSelect',
+    method: 'get'
+  })
+}
+
+export function getTableField(params) {
+  return request({
+    url: '/generate/generateCode/getTableField',
+    method: 'get',
+    params
+  })
+}
+export function generateCode(params) {
+  const url = 'http://localhost:8090/api-gateway/generate/generateCode/generateCode'
+  return XHRLoadLoadFile(url, params)
+}
+
+function XHRLoadLoadFile(url, data) {
+  const xhr = new XMLHttpRequest()
+  xhr.open('post', url)
+  // 如果需要请求头中这是token信息可以在这设置
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+  xhr.setRequestHeader('token', getToken())
+  xhr.responseType = 'blob'
+  xhr.send(JSON.stringify(data))
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const blob = new Blob([xhr.response])
+      const url = window.URL.createObjectURL(blob)
+      // 创建一个a标签元素，设置下载属性，点击下载，最后移除该元素
+      const link = document.createElement('a')
+      link.href = url
+      link.style.display = 'none'
+      // 取出下载文件名
+      // const fileName = xhr.getResponseHeader('filename')
+      const fileName = data.tableName + '.java'
+      const downlaodFileName = decodeURIComponent(fileName)
+      link.setAttribute('download', downlaodFileName)
+      link.click()
+      window.URL.revokeObjectURL(url)
+    }
+  }
+}
+
---
+++ src/views/generate/generateCode.vue
@@ -0,0 +1,136 @@
+<template>
+  <div style="padding:30px;">
+    <div class="search-div-bar">
+      <el-form ref="form" :model="form" label-width="100px">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <el-form-item label="数据库表">
+                <el-select v-model="form.tableName" placeholder="请选择数据库表" @change="tableNameSelectChange">
+                  <el-option
+                    v-for="item in tableOption"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <el-form-item label="仅生成Bean">
+                <el-switch
+                  v-model="form.onlyGenerateJavaBean"
+                />
+              </el-form-item>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <el-form-item label="包路径">
+                <el-input v-model="form.packagePath" />
+              </el-form-item>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <el-form-item label="是否需要分页">
+                <el-switch
+                  v-model="form.needPagination"
+                />
+              </el-form-item>
+            </div>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div v-if="fieldShow" class="field-div">
+      <div class="action-div-bar">
+        <el-button type="primary" icon="el-icon-finished" size="small" @click="generateCodeClick">生成代码</el-button>
+      </div>
+      <el-table
+        :data="tableFieldData"
+        style="width: 100%"
+        max-height="100%"
+      >
+        <el-table-column type="index" width="100" />
+        <el-table-column prop="dbColumnName" label="数据库字段名" width="260" />
+        <el-table-column prop="dbColumnType" label="数据库字段类型" width="260" />
+        <el-table-column prop="javaJavaHumpColumnName" label="Java驼峰字段名" width="260" />
+        <el-table-column prop="javaColumnType" label="Java字段类型" width="260" />
+        <el-table-column prop="paginationQueryCondition" label="是否作为分页查询条件" width="320" />
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getMysqlTableSelect, getTableField, generateCode } from '@/api/generateCode'
+
+export default {
+  name: 'GenerateCode',
+  components: {
+
+  },
+  data() {
+    return {
+      tableFieldData: [],
+      fieldShow: false,
+      form: {
+        tableName: '',
+        onlyGenerateJavaBean: false,
+        needPagination: false,
+        packagePath: ''
+      },
+      rules: {
+      },
+      formLabelWidth: '120px',
+      dialogFormVisible: false,
+      tableOption: []
+    }
+  },
+  mounted() {
+    // 获去数据库表列表
+    getMysqlTableSelect().then(res => {
+      this.tableOption = res.data
+    })
+  },
+  methods: {
+    tableNameSelectChange: function() {
+      this.fieldShow = true
+      const params = { tableName: this.form.tableName }
+      getTableField(params).then(res => {
+        this.tableFieldData = res.data
+      })
+    },
+    generateCodeClick: function() {
+      const params = {
+        tableName: this.form.tableName,
+        onlyGenerateJavaBean: this.form.onlyGenerateJavaBean,
+        packagePath: this.form.packagePath,
+        needPagination: this.form.needPagination,
+        tableFieldList: this.tableFieldData
+      }
+      generateCode(params)
+    }
+  }
+}
+</script>
+
+<style scope>
+  .search-div-bar {
+    margin-bottom: 20px;
+  }
+  .label-input {
+    padding-right: 50px;
+    color: #909399;
+  }
+  .searchParam-input {
+    width: 200px;
+  }
+  .pagination-div {
+    margin-top: 20px;
+    text-align: center;
+  }
+</style>
